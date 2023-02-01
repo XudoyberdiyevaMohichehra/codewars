e73d400eeb861f8054ac6e8b47d3be1bf8fea140
Mohichehra <xudoyberdiyevam25@gmail.com>
2023-02-01 21:32:37 +0300

    getCount(str)

--- codewars.js
+++ codewars.js
@@ -354,5 +354,19 @@
 
 
 
-const min = (list) => Math.min(...list);
-const max = (list) => Math.max(...list);
+// const min = (list) => Math.min(...list);
+// const max = (list) => Math.max(...list);
+
+function getCount(str) {
+    var vowelsCount = 0;
+    var vowels = ["a","e","i","o","u"];
+    for(var i = 0;i < str.length;i++){
+      for(var j=0;j<vowels.length;j++){
+        if(str[i] === vowels[j]){
+          vowelsCount++;
+        }
+      }
+    }
+    
+    return vowelsCount;
+  }
