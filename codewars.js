7d695c76f4422d6f4cf76605b5e3fe3309a29233
Mohichehra <xudoyberdiyevam25@gmail.com>
2023-02-28 23:05:27 +0500

    noSpace(x)

--- codewars.js
+++ codewars.js
@@ -533,20 +533,31 @@
 //     return format;
 //   }
 
-function countPositivesSumNegatives(input) {
-  if (input == null || input.length == 0)
-    return [];
+// function countPositivesSumNegatives(input) {
+//   if (input == null || input.length == 0)
+//     return [];
   
-  var positive = 0;
-  var negative = 0;
+//   var positive = 0;
+//   var negative = 0;
   
-  for (var i=0, l=input.length; i<l; ++i)
-  {
-    if (input[i] > 0)
-      ++ positive;
-    else
-      negative += input[i];
+//   for (var i=0, l=input.length; i<l; ++i)
+//   {
+//     if (input[i] > 0)
+//       ++ positive;
+//     else
+//       negative += input[i];
+//   }
+  
+//   return [positive, negative];
+// }
+
+
+function noSpace(x){
+  var result = ""
+  for(var index = 0; index < x.length; index++){
+    if(x[index] !== " "){
+      result += x[index];
+    }
   }
-  
-  return [positive, negative];
+  return result;
 }
