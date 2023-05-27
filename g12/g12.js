b4ee7b07e1a6d51519dc601aaf0803024e067e07
Mohichehra <xudoyberdiyevam25@gmail.com>
2023-05-27 10:39:16 +0500

    27.05.2023   4/5

--- g12/g12.js
+++ g12/g12.js
@@ -152,16 +152,16 @@
 
 // 13.05.2023   3/10
 
-// 1. 
+// 1.
 
 // function hasUniqueChars(str){
 //     return new Set(str).size === str.length
-   
+
 // }
 
 // console.log(hasUniqueChars("n??Aa++"));
 
-// 2 
+// 2
 // var str = ' zyxwvutsrqponmlkjihgfedcba!? '
 // function switcher(x) {
 //   return x.map((vl)=> {return str[vl]}).join('')
@@ -169,7 +169,79 @@
 
 // console.log(switcher(['24', '12', '23', '22', '4', '26', '9', '8']));
 
- 
-import { ggg } from "./g12";
-console.log(ggg);
-console.log(1);
+
+// 13.05.2023   3/10
+
+// 1. ?>!*/ => soz ichida  shu kabi belgilar bittadan oshsa false oshmasa true qaytaradigan funksiya yozing
+
+// function UniqueChar(str){
+//
+// }
+
+// console.log(UniqueChar("n??Aa++")); false
+// console.log(UniqueChar("n?Aa+")); true
+
+// =========================================================================================================
+
+// 2 Array ichidagi raqamlardan quyidagi ko'rinishdagi so'z qaytsin
+// function switcher(x) {
+//
+// }
+// console.log(switcher(['24', '12', '23', '22', '4', '26', '9', '8'])); =>' codewars'
+// console.log(switcher([ '26', '9', '22'])); => 'are'
+
+// =========================================================================================================
+// 3. str ichidagi sozlarni oxirgi harfiga qarab alifbo tartibida joylashtiring
+
+// last('man i need a taxi up to ubud') => ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']
+
+// =========================================================================================================
+// 4 dasturlash tillarini o'sish tartibiiga kora tertiblang. lekin ular 60 dan kam kursatgichga ega bulsa kursatilmasin
+
+// myLanguages({ "Java": 10, "Ruby": 80, "Python": 65 }) => ["Ruby", "Python"]
+// myLanguages({ "Hindi": 60, "Greek": 71, "Dutch": 93 }), => ["Dutch", "Greek", "Hindi"]
+// myLanguages({ "C++": 50, "ASM": 10, "Haskell": 20 }), => []
+
+// =========================================================================================================
+
+// 5 array ichidagi faqat juft sonlar qaytsin
+
+// getEvenNumbers([1, 2, 3, 6, 8, 10]), =>[2, 6, 8, 10]
+// getEvenNumbers([1, 2]),=> [2]
+
+// =========================================================================================================
+
+// 6 array ichidagi nested arraylar yuqotilsin
+
+// cut([[], [1]]),=> [1]
+// cut([[3, 2, 1],=> [7, 9, 8], [6, 4, 5]]), [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// =========================================================================================================
+
+// 7 array ichidagi positive va negative sonlarni alohida qushing
+
+// Example
+// For input[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+// =========================================================================================================
+
+// 8 array ichidagi positive sonlar negativega aylansin va negative sonlar positive sonlarga aylansin.
+
+// convert([1, 2, 3, 4, 5]) == [-1, -2, -3, -4, -5]
+// convert([1, -2, 3, -4, 5]) == [-1, 2, -3, 4, -5]
+
+// =========================================================================================================
+
+// 9 asrlarni tugri hisoblaydigan ffunksiya yozing
+
+// asr(1601), => 17
+// asr(1705), => 18
+
+// =========================================================================================================
+
+// 10 Nomlarni qisqartiring
+
+// Shorten("Sam Harris"), => "S.H"
+// abbrevName("Patrick Feenan"), => "P.F"
+
+
--- g12/second.js
+++ g12/second.js
@@ -1,76 +1,40 @@
-// 13.05.2023   3/10
+// 27.05.2023   4/5
 
-// 1. ?>!*/ => soz ichida  shu kabi belgilar bittadan oshsa false oshmasa true qaytaradigan funksiya yozing
+// 1. returnga unique value qaytsin.
 
-// function UniqueChar(str){
-//
-// }
+// findUniq([1, 1, 1, 2, 1, 1]) === 2
+// findUniq([0, 0, 0.55, 0, 0]) === 0.55
 
-// console.log(UniqueChar("n??Aa++")); false
-// console.log(UniqueChar("n?Aa+")); true
+// ============================================================================
 
-// =========================================================================================================
+// 2 toq valularni tartiblang
 
-// 2 Array ichidagi raqamlardan quyidagi ko'rinishdagi so'z qaytsin
-// function switcher(x) {
-//
-// }
-// console.log(switcher(['24', '12', '23', '22', '4', '26', '9', '8'])); =>' codewars'
-// console.log(switcher([ '26', '9', '22'])); => 'are'
+// [7, 1]  => [1, 7]
+// [5, 8, 6, 3, 4]  => [3, 8, 6, 5, 4]
+// [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  => [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
 
-// =========================================================================================================
-// 3. str ichidagi sozlarni oxirgi harfiga qarab alifbo tartibida joylashtiring
+// ============================================================================
 
-// last('man i need a taxi up to ubud') => ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']
+// 3. valuelar yigindisini toping
 
-// =========================================================================================================
-// 4 dasturlash tillarini o'sish tartibiiga kora tertiblang. lekin ular 60 dan kam kursatgichga ega bulsa kursatilmasin
+// arrayPlusArray([1, 2, 3], [4, 5, 6]), 21
+// arrayPlusArray([-1, -2, -3], [-4, -5, -6]), -21
+// arrayPlusArray([0, 0, 0], [4, 5, 6]), 15
+// arrayPlusArray([100, 200, 300], [400, 500, 600]), 2100
 
-// myLanguages({ "Java": 10, "Ruby": 80, "Python": 65 }) => ["Ruby", "Python"]
-// myLanguages({ "Hindi": 60, "Greek": 71, "Dutch": 93 }), => ["Dutch", "Greek", "Hindi"]
-// myLanguages({ "C++": 50, "ASM": 10, "Haskell": 20 }), => []
+// ============================================================================
 
-// =========================================================================================================
+// 4. valuelar o'rta arifmetigini toping
 
-// 5 array ichidagi faqat juft sonlar qaytsin
+// (getAverage([2, 2, 2, 2]), 2);
+// (getAverage([1, 2, 3, 4, 5,]), 3);
+// (getAverage([1, 1, 1, 1, 1, 1, 1, 2]), 1)
 
-// getEvenNumbers([1, 2, 3, 6, 8, 10]), =>[2, 6, 8, 10]
-// getEvenNumbers([1, 2]),=> [2]
+// ============================================================================
 
-// =========================================================================================================
+// 5. valuelarni har ikkinchi yani cherez  malumotlarini chiqarib tashlang
 
-// 6 array ichidagi nested arraylar yuqotilsin
-
-// cut([[], [1]]),=> [1]
-// cut([[3, 2, 1],=> [7, 9, 8], [6, 4, 5]]), [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-// =========================================================================================================
-
-// 7 array ichidagi positive va negative sonlarni alohida qushing
-
-// Example
-// For input[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
-
-// =========================================================================================================
-
-// 8 array ichidagi positive sonlar negativega aylansin va negative sonlar positive sonlarga aylansin.
-
-// convert([1, 2, 3, 4, 5]) == [-1, -2, -3, -4, -5]
-// convert([1, -2, 3, -4, 5]) == [-1, 2, -3, 4, -5]
-
-// =========================================================================================================
-
-// 9 asrlarni tugri hisoblaydigan ffunksiya yozing
-
-// asr(1601), => 17
-// asr(1705), => 18
-
-// =========================================================================================================
-
-// 10 Nomlarni qisqartiring
-
-// Shorten("Sam Harris"), => "S.H"
-// abbrevName("Patrick Feenan"), => "P.F"
-
-
-export let ggg = 'asdcfv'
+// (removeEveryOther(['Hello', 'Goodbye', 'Hello Again']),=> ['Hello', 'Hello Again']);
+// (removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),=>  [1, 3, 5, 7, 9]);
+// (removeEveryOther([[1, 2]]),=>  [[1, 2]]);
+// (removeEveryOther([['Goodbye'], { 'Great': 'Job' }]),=>  [['Goodbye']]);
